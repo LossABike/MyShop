32c624995b1fba1729db9942a847c25dbdc2907e
HieuNgO <ngoduchieuxxx@gmail.com>
2022-09-22 15:10:43 +0700

    add topright badge

--- lib/ui/products/products_overview_screen.dart
+++ lib/ui/products/products_overview_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import '../cart/cart_screen.dart';
 import '../shared/app_drawer.dart';
 import 'products_grid.dart';
+import '../cart/cart_manager.dart';
+import 'top_right_badge.dart';
 
 enum FilterOptions { favorites, all }
 
@@ -31,13 +33,24 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   }
 
   Widget buildShoppingCartIcon() {
-    return IconButton(
-      icon: const Icon(
-        Icons.shopping_cart,
-      ),
-      onPressed: () {
-        Navigator.of(context).pushNamed(CartScreen.routeName);
-      },
+    // return IconButton(
+    //   icon: const Icon(
+    //     Icons.shopping_cart,
+    //   ),
+    //   onPressed: () {
+    //     Navigator.of(context).pushNamed(CartScreen.routeName);
+    //   },
+    // );
+    return TopRightBadge(
+        data: CartManager().productCount,
+        child: IconButton(
+          icon: const Icon(
+            Icons.shopping_cart,
+          ),
+          onPressed: () {
+            Navigator.of(context).pushNamed(CartScreen.routeName);
+          },
+        ),
     );
   }
 
